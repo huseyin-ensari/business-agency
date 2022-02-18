17680e67acd61172a5570f5179e4fa2e715f86bb
Hüseyin Ensari <hsynensari@gmail.com>
2022-02-18 05:09:05 +0300

    Make custom video controls work

--- app.js
+++ app.js
@@ -1,8 +1,42 @@
+// navbar
 const menu = document.querySelector(".menu");
 const navbar = document.querySelector(".navbar");
-//
 
 menu.addEventListener("click", () => {
   navbar.classList.toggle("change");
   menu.classList.toggle("change");
 });
+// end of navbar
+
+// section 2 video
+
+const video = document.querySelector(".video");
+const btn = document.querySelector(".buttons button i");
+const bar = document.querySelector(".video-bar");
+
+const playPause = () => {
+  if (video.paused) {
+    video.play();
+    btn.className = "far fa-pause-circle";
+    video.style.opacity = ".7";
+  } else {
+    video.pause();
+    btn.className = "far fa-play-circle";
+    video.style.opacity = ".3";
+  }
+};
+
+btn.addEventListener("click", () => {
+  playPause();
+});
+
+video.addEventListener("timeupdate", () => {
+  console.log(video.currentTime, video.duration);
+  const barWidth = video.currentTime / video.duration;
+  bar.style.width = `${barWidth * 100}%`;
+  if (video.ended) {
+    btn.className = "far fa-play-circle";
+    video.style.opacity = ".3";
+  }
+});
+// end of section 2 video
